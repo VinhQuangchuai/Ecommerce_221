a6f433d7b2f45811f98390287c436b495c1bd3c6
Tran Minh Tan <82354057+minhtantran1608@users.noreply.github.com>
2022-11-23 17:47:04 +0700

    merge with main

--- src/App.js
+++ src/App.js
@@ -1,55 +1,52 @@
-
-import React, {useState, useEffect, Fragment} from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import './App.css';
-import { publishRoutes } from './routes';
-import { ProductContext } from './components/Context';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { fetchPosts } from './api/product';
+import React, { useState, useEffect, Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { publishRoutes, privateRoutes } from "./routes";
+import { ProductContext } from "./components/Context";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { fetchPosts } from "./api/product";
 function App() {
-
-  const [page, setPage] = useState(1)
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    fetchPosts().then((response) => setData(response.data))
-}, [])
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-      window.scrollTo({
-          top: 0,
-          behavior: "smooth"
-        });
-  },[page])
+    fetchPosts().then((response) => setData(response.data));
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [page]);
 
   const handleClick = () => {
     if (page === 1) {
-      setPage(0)
-    }
-    else setPage(1)
-  }
+      setPage(0);
+    } else setPage(1);
+  };
   return (
     <Router>
       <div className="App">
-       <ProductContext.Provider value={{data, setData}}>
-        <Header/>
-         <Routes>
-          {
-            publishRoutes.map((route, id) => {
-              const Page = route.component
-              return (
-                <Route key={id} path={route.path} element={<Page/>}/>
-              )
-            })
-          }
-         </Routes>
-         <Footer/>
+        <ProductContext.Provider value={{ data, setData }}>
+          <Header />
+          <Routes>
+            {publishRoutes.map((route, id) => {
+              const Page = route.component;
+              return <Route key={id} path={route.path} element={<Page />} />;
+            })}
+            {privateRoutes.map((route, id) => {
+              const Page = route.component;
+              return <Route key={id} path={route.path} element={<Page />} />;
+            })}
+          </Routes>
+          <Footer />
 
-          <p className='to-top' onClick={handleClick}>
-              <i className="fa-sharp fa-solid fa-chevron-up"></i>
+          <p className="to-top" onClick={handleClick}>
+            <i className="fa-sharp fa-solid fa-chevron-up"></i>
           </p>
-       </ProductContext.Provider>
+        </ProductContext.Provider>
       </div>
     </Router>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,12 @@
 import { HomePage } from "../pages/Homepage";
 import { Defaultlayout } from "../components/DefaultLayout";
 import { ProductPage } from "../pages/ProductPage";
-
+import Checkout from "../pages/Checkout/Checkout";
 
 export const publishRoutes = [
-    { path: '/', component: HomePage },
-    { path: '/shop', component: Defaultlayout},
-    { path: '/shop/:id', component: ProductPage },
-    
-
-    
+  { path: "/", component: HomePage },
+  { path: "/shop", component: Defaultlayout },
+  { path: "/shop/:id", component: ProductPage },
 ];
 
-export const privateRoutes = [];
+export const privateRoutes = [{ path: "/checkout", component: Checkout }];
