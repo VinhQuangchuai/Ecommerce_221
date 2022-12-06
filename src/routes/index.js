4993d716fd6a419746741abee51e5db830a2383d
HoangLC19 <hoanglc1911@gmail.com>
2022-12-06 19:23:07 +0700

    "update payment fail route"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 import { HomePage } from "../pages/Homepage";
 import { Defaultlayout } from "../components/DefaultLayout";
 import { ProductPage } from "../pages/ProductPage";
-import {Checkout} from "../pages/Checkout/index";
-import {PaymentFail} from "../pages/PaymentFail";
-import {PaymentSuccess} from "../pages/PaymentSuccess";
+import { Checkout } from "../pages/Checkout/index";
+import { PaymentFail } from "../pages/PaymentFail";
+import { PaymentSuccess } from "../pages/PaymentSuccess";
 
 export const publishRoutes = [
   { path: "/", component: HomePage },
   { path: "/shop", component: Defaultlayout },
   { path: "/shop/:id", component: ProductPage },
-  { path: "/payment-failed", component: PaymentFail },
+  { path: "/payment-fail", component: PaymentFail },
   { path: "/payment-success", component: PaymentSuccess },
 
 ];
