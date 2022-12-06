9cd663a8971219038fde47637665c763e0dc9941
HoangLC19 <hoanglc1911@gmail.com>
2022-12-06 18:56:56 +0700

    "edit purchase, add payment success and fail page"

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,16 @@ import { HomePage } from "../pages/Homepage";
 import { Defaultlayout } from "../components/DefaultLayout";
 import { ProductPage } from "../pages/ProductPage";
 import {Checkout} from "../pages/Checkout/index";
+import {PaymentFail} from "../pages/PaymentFail";
+import {PaymentSuccess} from "../pages/PaymentSuccess";
 
 export const publishRoutes = [
   { path: "/", component: HomePage },
   { path: "/shop", component: Defaultlayout },
   { path: "/shop/:id", component: ProductPage },
+  { path: "/payment-failed", component: PaymentFail },
+  { path: "/payment-success", component: PaymentSuccess },
+
 ];
 
 export const privateRoutes = [{ path: "/checkout", component: Checkout }];
